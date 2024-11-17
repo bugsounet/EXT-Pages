3e40acf27e832cc582ca763ad2bdc904c1cd2207
bugsounet <github@bugsounet.fr>
2024-11-17 15:27:54 +0100

    lint: node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -3,7 +3,7 @@
 var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
-  socketNotificationReceived (noti, payload) {
+  socketNotificationReceived (noti) {
     switch (noti) {
       case "INIT":
         console.log("[PAGES] EXT-Pages Version:", require("./package.json").version, "rev:", require("./package.json").rev);
